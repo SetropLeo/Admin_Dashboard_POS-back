ab8d339583a8946530ebbae10c4b618a8c46be42
LeonardoPortes <leol_portes@hotmail.com>
2022-09-07 17:49:27 -0300

    Add new user when starting new database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import { AppDataSource } from "./database/data-source";
+import { User } from "./entities/User";
 
 AppDataSource.initialize()
-  .then(() => {
-    console.log('Migrations Runned');
+  .then(async () => {
+
+    const user = new User();
+    user.first_name = 'Leo';
+    user.last_name = 'Setrop';
+    user.email = 'leol_portes@hotmail.com';
+    user.password = '123';
+    await AppDataSource.manager.save(user);
+
+    console.log('Load users from the database');
+    const users = await AppDataSource.manager.find(User);
+    console.log('Loaded Users', users);
+
+
+    console.log('Inserted informations in the database.');
   })
   .catch((error) => console.log(error))
